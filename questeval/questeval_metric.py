26b7909a5357965d8551fec33d6ce938d56f9fd0
thomasScialom <thomas@recital.ai>
2021-04-09 13:45:16 +0200

    fixing assert

--- questeval/questeval_metric.py
+++ questeval/questeval_metric.py
@@ -340,14 +340,14 @@ class QuestEval:
             f1_scores = list_borned(f1_scores)
             answ_scores = list_borned(answ_scores)
             if self.do_BERTScore:
-                assert bert_scores is not None, 'BERTScore is None while do_BERTScore is activated'
+                assert bert_scores is not None, 'bert_scores is None while do_BERTScore is activated'
                 bert_scores = list_borned(bert_scores)
 
             if self.do_consistency:
                 assert ques_consists is not None, "ques_consists is None. Please compute the score with ques_consists activate."
                 f1_scores, answ_scores, bert_scores = regularizer(f1_scores, answ_scores, bert_scores, ques_consists)
 
-            if self.do_weighter is not None:
+            if self.do_weighter:
                 assert weighter_probs is not None, "weighter_probs is None. Please compute the score with ques_consists activate."
                 f1_scores, answ_scores, bert_scores = regularizer(f1_scores, answ_scores, bert_scores, weighter_probs)
 
